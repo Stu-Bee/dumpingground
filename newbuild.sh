d4329ac6df74c6e71229eb899a9ec76739da3443
Stu Burnside <39988555+Stu-Bee@users.noreply.github.com>
2019-10-14 16:51:57 +0100

    Create newbuild.sh

---
+++ newbuild.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install node
+brew install wget
+brew install z
+brew install ag
+brew install ack
+brew install ffind
+brew install fpp
+brew install tmux
+brew install watchman
+brew install yarn
+
+npm install -g ipt
+npm install -g http-server
+npm install -g esformatter
+npm install -g prettier
+npm install -g eslint
+npm install -g replace
+
+# essential
+brew cask install 1password
+
+# dev
+brew cask install iterm2
+brew cask install macvim
+brew cask install virtualbox
+brew cask install vagrant
+
+# utils
+brew cask install the-unarchiver
+
+# browsers
+brew cask install firefox
+brew cask install google-chrome
